c446090606ca56829e044b8ede350b62f1864a77
Danniel Cristie <dany14012@gmail.com>
2022-12-02 10:54:58 -0300

    route with mock response

---
+++ src/api/todos/todos.routes.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from 'express';
+import Todo from './todos.model';
+
+const router = Router();
+
+router.get('/', (req: Request, res: Response<Todo[]>) => {
+  res.json([{
+    content: ' Learn TypeScript',
+    done: false,
+  }]);
+});
+
+export default router;
