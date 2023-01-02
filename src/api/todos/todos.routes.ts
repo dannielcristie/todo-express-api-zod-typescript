dba2d29710e8546660a09eaecb42fbcbdab2d12b
Danniel Cristie <dany14012@gmail.com>
2023-01-02 15:59:56 -0300

    feat: to do route conncted mongo

--- src/api/todos/todos.routes.ts
+++ src/api/todos/todos.routes.ts
@@ -1,13 +1,12 @@
 import { Router, Request, Response } from 'express';
-import Todo from './todos.model';
+import { TodoWithId, Todos } from './todos.model';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response<Todo[]>) => {
-  res.json([{
-    content: ' Learn TypeScript',
-    done: false,
-  }]);
+router.get('/', async (req: Request, res: Response<TodoWithId[]>) => {
+  const result = await Todos.find();
+  const todos = await result.toArray();
+  res.json(todos);
 });
 
 export default router;
