7997b32e003350c10dd86ec2ec1432446d149c2d
Danniel Cristie <dany14012@gmail.com>
2023-01-02 16:26:38 -0300

    chore: error handler findAll

--- src/api/todos/todos.handler.ts
+++ src/api/todos/todos.handler.ts
@@ -1,9 +1,12 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { TodoWithId, Todos } from './todos.model';
 
-export const findAll = async (req: Request, res: Response<TodoWithId[]>) => {
-  const result = await Todos.find();
-  const todos = await result.toArray();
-  res.json(todos);
+export const findAll = async (req: Request, res: Response<TodoWithId[]>, next: NextFunction) => {
+  try {
+    const result = await Todos.find();
+    const todos = await result.toArray();
+    res.json(todos);
+  } catch (error) {
+    next(error);
+  };
 };
-
