ff39ea6bef06a985741b6740241ff432d7df42f7
Danniel Cristie <dany14012@gmail.com>
2023-01-02 22:18:09 -0300

    feat: create One todo

--- src/api/todos/todos.handler.ts
+++ src/api/todos/todos.handler.ts
@@ -1,5 +1,7 @@
+import { InsertOneResult } from 'mongodb';
 import { Request, Response, NextFunction } from 'express';
-import { TodoWithId, Todos } from './todos.model';
+import { TodoWithId, Todos, Todo } from './todos.model';
+import { ZodError } from 'zod';
 
 export const findAll = async (req: Request, res: Response<TodoWithId[]>, next: NextFunction) => {
   try {
@@ -10,3 +12,25 @@ export const findAll = async (req: Request, res: Response<TodoWithId[]>, next: N
     next(error);
   };
 };
+
+
+
+export const createOne = async (req: Request<{}, TodoWithId, Todo>, res: Response<TodoWithId>, next: NextFunction) => {
+  try {
+    const validateResult = await Todo.parseAsync(req.body);
+    const insertResult = await Todos.insertOne(validateResult);
+    if (!insertResult.acknowledged) throw new Error('Error inserting todo');
+    res.status(201);
+    res.json({
+      _id: insertResult.insertedId,
+      ...validateResult
+    });
+  } catch (error) {
+    if (error instanceof ZodError) {
+      res.status(422);
+    }
+    next(error);
+  };
+};
+
+
--- src/api/todos/todos.model.ts
+++ src/api/todos/todos.model.ts
@@ -4,7 +4,7 @@ import * as z from 'zod';
 import { db } from '../../db';
 
 
-const Todo = z.object({
+export const Todo = z.object({
   content: z.string().min(1),
   done: z.boolean().default(false),
 });
