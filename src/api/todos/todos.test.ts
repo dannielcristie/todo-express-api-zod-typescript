6a8d626ba71d31115e147b52a34e4b17e1ad2cd2
Danniel Cristie <dany14012@gmail.com>
2023-01-03 19:53:56 -0300

    test:  find one route tests

--- src/api/todos/todos.test.ts
+++ src/api/todos/todos.test.ts
@@ -23,6 +23,8 @@ describe('GET /api/v1/todos', () => {
   );
 });
 
+let id = '';
+
 describe('POST /api/v1/todos', () => {
   it('responds with an error if the todos is invalid', async () =>
     request(app)
@@ -50,10 +52,43 @@ describe('POST /api/v1/todos', () => {
       .expect(201)
       .then((response) => {
         expect(response.body).toHaveProperty('_id');
+        id = response.body._id;
+        expect(response.body).toHaveProperty('content');
+        expect(response.body.content).toBe('Learn Typescript');
+        expect(response.body).toHaveProperty('done');
+      })
+  );
+});
+
+describe('GET /api/v1/todos/:id', () => {
+  it('responds with a single todo', async () =>
+    request(app)
+      .get(`/api/v1/todos/${id}`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toHaveProperty('_id');
+        expect(response.body._id).toBe(id);
         expect(response.body).toHaveProperty('content');
         expect(response.body.content).toBe('Learn Typescript');
         expect(response.body).toHaveProperty('done');
       })
   );
 
+  it('responds with an invalid ObjectId error', (done) => {
+    request(app)
+      .get(`/api/v1/todos/anyId`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422, done)
+  });
+
+  it('responds with a not found error', (done) => {
+    request(app)
+      .get(`/api/v1/todos/63b4b11a649d50bd73a2ea22`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done)
+  });
 });
