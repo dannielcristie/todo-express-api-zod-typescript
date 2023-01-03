fb0ca14533a960d69b43a7c767220a457f58e21f
Danniel Cristie <dany14012@gmail.com>
2023-01-03 20:55:58 -0300

    test: delete one route tests

--- src/api/todos/todos.test.ts
+++ src/api/todos/todos.test.ts
@@ -133,3 +133,35 @@ describe('PUT /api/v1/todos/:id', () => {
       })
   );
 });
+
+describe('DELETE /api/v1/todos/:id', () => {
+  it('responds with an invalid ObjectId error', (done) => {
+    request(app)
+      .delete(`/api/v1/todos/anyId`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(422, done)
+  });
+
+  it('responds with a not found error', (done) => {
+    request(app)
+      .delete(`/api/v1/todos/63b4b11a649d50bd73a2ea22`)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(404, done)
+  });
+
+  it('responds with a 204 status code', (done) => {
+    request(app)
+      .delete(`/api/v1/todos/${id}`)
+      .set('Accept', 'application/json')
+      .expect(204, done)
+  });
+
+  it('responds with a not found error', (done) => {
+    request(app)
+      .get(`/api/v1/todos/${id}`)
+      .set('Accept', 'application/json')
+      .expect(404, done)
+  })
+});
